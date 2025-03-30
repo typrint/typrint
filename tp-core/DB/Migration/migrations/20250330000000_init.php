767d6f8ad5ebd457a88e2a87f97179de89a8e498
耗子 <haozi@loli.email>
2025-03-30 21:08:25 +0800

    feat: optimize database index

--- tp-core/DB/Migration/migrations/20250330000000_init.php
+++ tp-core/DB/Migration/migrations/20250330000000_init.php
@@ -28,8 +28,8 @@ class Init extends AbstractMigration
             ->addColumn('url', 'string', ['limit' => 255])
             ->addColumn('status', 'string', ['limit' => 20])
             ->addTimestamps()
-            ->addIndex('name', ['unique' => true, 'name' => 'idx_'.DB_TABLE_PREFIX.'users_name'])
-            ->addIndex('email', ['unique' => true, 'name' => 'idx_'.DB_TABLE_PREFIX.'users_email'])
+            ->addIndex('name', ['unique' => true, 'name' => 'uk_'.DB_TABLE_PREFIX.'users_name'])
+            ->addIndex('email', ['unique' => true, 'name' => 'uk_'.DB_TABLE_PREFIX.'users_email'])
             ->addIndex('display_name', ['name' => 'idx_'.DB_TABLE_PREFIX.'users_display_name'])
             ->addIndex('status', ['name' => 'idx_'.DB_TABLE_PREFIX.'users_status'])
             ->create();
@@ -68,10 +68,10 @@ class Init extends AbstractMigration
             ->addColumn('status', 'string', ['limit' => 20])
             ->addColumn('password', 'string', ['limit' => 255])
             ->addTimestamps()
-            ->addIndex(['name', 'type'], ['unique' => true, 'name' => 'idx_'.DB_TABLE_PREFIX.'contents_name_type'])
+            ->addIndex(['name', 'type'], ['unique' => true, 'name' => 'uk_'.DB_TABLE_PREFIX.'contents_name_type'])
             ->addIndex(['parent', 'type', 'status'], ['name' => 'idx_'.DB_TABLE_PREFIX.'contents_parent_type_status'])
-            ->addIndex(['type', 'status', 'created_at', 'user_id'], ['name' => 'idx_'.DB_TABLE_PREFIX.'contents_type_status_created_at'])
-            ->addIndex(['user_id', 'type', 'status', 'created_at'], ['name' => 'idx_'.DB_TABLE_PREFIX.'contents_user_id_type_status'])
+            ->addIndex(['type', 'status', 'created_at', 'user_id'], ['name' => 'idx_'.DB_TABLE_PREFIX.'contents_type_status_created_at_user_id'])
+            ->addIndex(['user_id', 'type', 'status', 'created_at'], ['name' => 'idx_'.DB_TABLE_PREFIX.'contents_user_id_type_status_created_at'])
             ->create();
 
         $this->table(DB_TABLE_PREFIX.'contentmeta', ['id' => false, 'primary_key' => ['id']])
@@ -107,7 +107,7 @@ class Init extends AbstractMigration
             ->addColumn('description', 'text', ['limit' => MysqlAdapter::TEXT_LONG])
             ->addColumn('count', 'biginteger', ['default' => 0])
             ->addIndex('taxonomy', ['name' => 'idx_'.DB_TABLE_PREFIX.'term_taxonomy_taxonomy'])
-            ->addIndex(['term_id', 'taxonomy'], ['unique' => true, 'name' => 'idx_'.DB_TABLE_PREFIX.'term_taxonomy_term_id_taxonomy'])
+            ->addIndex(['term_id', 'taxonomy'], ['unique' => true, 'name' => 'uk_'.DB_TABLE_PREFIX.'term_taxonomy_term_id_taxonomy'])
             ->create();
 
         $this->table(DB_TABLE_PREFIX.'term_relationships', ['id' => false, 'primary_key' => ['content_id', 'term_taxonomy_id']])
